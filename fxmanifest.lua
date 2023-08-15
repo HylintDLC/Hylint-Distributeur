95d86f7f3be97e0aa2b360e9a7318e343710ecb0
HylintDLC <141586153+HylintDLC@users.noreply.github.com>
2023-08-15 15:48:10 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+---@diagnostic disable: undefined-global
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 'yes'
+
+author 'hylint'
+description 'Hylint Vending Machine ESX.'
+version '1.5'
+
+dependencies { 'ox_lib', 'ox_target', 'ox_inventory'}
+
+shared_scripts {'@ox_lib/init.lua'}
+
+server_scripts {
+	'server.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
