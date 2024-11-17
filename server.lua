ee115019368a7b188187e7ad7da23a92d28496d4
HylintDLC <141586153+HylintDLC@users.noreply.github.com>
2024-11-17 14:55:40 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,39 +4,87 @@
 
 local ox_inventory = exports.ox_inventory
 
---------- Coffee Machines
-local itemName = {
-	'Café Noir', 'Café au Lait', 'Café Sucré', 'Café Crème', -- Coffees, 1-4
-	'Sprunk', 'Sprunk-Light', 'Orang-O-Tang', 'eCola', 'Diet-eCola' -- Sodas 5-9
-}
-
 local classes = {
-	['coffee'] = {
-		waitTime = 2000,
-		cost = 3
-	},
-	['can'] = {
-		waitTime = 4000,
-		cost = 2
-	},
 	['water'] = {
 		waitTime = 2000,
 		cost = 1
 	},
+	['sprunk'] = {
+		waitTime = 4000,
+		cost = 2
+	},
+	['ejunk'] = {
+		waitTime = 4000,
+		cost = 2
+	},
+	['orangotang'] = {
+		waitTime = 4000,
+		cost = 2
+	},
+	['ecola'] = {
+		waitTime = 4000,
+		cost = 2
+	},
+	['ecoladiet'] = {
+		waitTime = 4000,
+		cost = 2
+	},
+	['coffee_black'] = {
+		waitTime = 2000,
+		cost = 3
+	},
+	['coffee_milked'] = {
+		waitTime = 2000,
+		cost = 3
+	},
+	['coffee_sugar'] = {
+		waitTime = 2000,
+		cost = 3
+	},
+	['coffee_mocha'] = {
+		waitTime = 2000,
+		cost = 3
+	},
+	['coffee_cpcno'] = {
+		waitTime = 2000,
+		cost = 3
+	},
+	['coffee_amrcno'] = {
+		waitTime = 2000,
+		cost = 3
+	},
 }
 
-RegisterServerEvent('hylintvending:buy', function(type, name)
+RegisterServerEvent('hylintvending:buy', function(type)
 	local src = source
 	Wait(classes[type].waitTime)
 	local count = ox_inventory:Search(src, 'count', 'money')
 	if count >= classes[type].cost then
 		ox_inventory:RemoveItem(src, 'money', classes[type].cost)
-		if type == 'coffee' then
-			ox_inventory:AddItem(src, 'coffee', 1, {label = itemName[name], description = 'Un '..itemName[name]})
-		elseif type == 'can' then
-			ox_inventory:AddItem(src, 'can', 1, {label = itemName[name], description = 'Une cannette de '..itemName[name], image = itemName[name]})
-		elseif type == 'water' then
-			ox_inventory:AddItem(src, 'water', 1)
+	    if type == 'water' then
+	    	ox_inventory:AddItem(src, 'water', 1)
+		elseif type == 'sprunk' then
+			ox_inventory:AddItem(src, 'sprunk', 1)
+		elseif type == 'ejunk' then
+			ox_inventory:AddItem(src, 'ejunk', 1)
+		elseif type == 'orangotang' then
+			ox_inventory:AddItem(src, 'orangotang', 1)
+		elseif type == 'ecola' then
+			ox_inventory:AddItem(src, 'ecola', 1)
+		elseif type == 'ecoladiet' then
+			ox_inventory:AddItem(src, 'ecoladiet', 1)
+		elseif type == 'coffee_black' then
+			ox_inventory:AddItem(src, 'coffee_black', 1)
+		elseif type == 'coffee_milked' then
+			ox_inventory:AddItem(src, 'coffee_milked', 1)
+		elseif type == 'coffee_sugar' then
+			ox_inventory:AddItem(src, 'coffee_sugar', 1)
+		elseif type == 'coffee_mocha' then
+			ox_inventory:AddItem(src, 'coffee_mocha', 1)
+		elseif type == 'coffee_cpcno' then
+			ox_inventory:AddItem(src, 'coffee_cpcno', 1)
+		elseif type == 'coffee_amrcno' then
+			ox_inventory:AddItem(src, 'coffee_amrcno', 1)
 		end
 	else
 		TriggerClientEvent('ox_lib:notify', src, {
